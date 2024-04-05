6f7bea6fd6901f6d7fc0cf0bb3162f5791ce43d2
eglkas <158996561+eglkas@users.noreply.github.com>
2024-04-05 13:58:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+/* ------------------------------ TASK 7 -----------------------------------
+Turimas "audi" objektas.
+
+Parašykite funkciją showObjectKeys, kuri kaip argumentą priims objectą 
+ir grąžins visus jo "key" masyve.
+-------------------------------------------------------------------------- */
+const audi = {
+  make: 'audi',
+  model: 'A6',
+  year: 2005,
+  color: 'white',
+};
+const showObjectKeysMain = Object.keys(audi);
+const showObjectKeys = Object.keys(audi).map((key) => [(key), audi[key]]);
